1f5cf72b4137bda246715122cbab061c3985fce6
Zhanglin Wu <zhang-lin.wu@perkinelmer.com>
2018-11-02 19:40:53 +0800

    DSK-18281 Update the add-in developer guide and examples for the newly added "Selection" API (#10)
    Here are the updates:
    1. Add description into the developer guide for the newly added API "Selection"
    2. Add an add-in example to demonstrate the usage of the newly added API
    3. Update the snapshots of the existing add-in examples so that they are consistent with the newly added add-in example which is using Windows 10 style dialogs

---
+++ Examples/Selection Monitor/selection-monitor.js
@@ -0,0 +1,32 @@
+//
+// selection-monitor.js
+//
+// This is an example of using ChemDraw Add-in API to get the selection in a document.
+//
+// Copyright (c) 2018 PerkinElmer, Inc. All rights reserved.
+
+// ESLint configuration
+/* global ChemDrawAPI */
+
+function showSelection() {
+    var messageBox = document.getElementById('message-box');
+    if (ChemDrawAPI.activeDocument.selection.isEmpty()) {
+        messageBox.innerHTML = 'Please select a structure';
+    }
+    else {
+        messageBox.innerHTML = ChemDrawAPI.activeDocument.selection.getSVG({
+            transparent: true,
+            scalePercent: 100,
+            borderSizeInPixels: 5
+        });
+    }
+}
+
+function onLoadBody() {
+    ChemDrawAPI.window.resizeTo(400, 480);
+    showSelection();
+}
+
+ChemDrawAPI.activeDocument.selection.onChange(function () {
+    showSelection();
+});
